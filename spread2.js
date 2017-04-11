473bfee9d56078ae67ff54702a04a99f56261c91
Michael Santo Domingo <msd2802@gmail.com>
2017-04-11 12:11:35 +0000

    two spread methods

---
+++ spread2.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const newNumbers = [
+  // include all the items in the numbers array
+  // without the enclosing array
+  ...numbers,
+  10
+];
+
+console.log(newNumbers);
